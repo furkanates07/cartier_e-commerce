ecef86e6f80d1e145771476f0fb935369568ae5f
furkanates <furkanates4@posta.mu.edu.tr>
2024-08-07 04:41:07 +0300

    chore: edit footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="border mt-auto p-2 text-center flex flex-col gap-1">
+  <footer class="border mt-auto p- text-center flex flex-col gap-1">
     <span>cartier | All rights reserved. 2020-2024</span>
     <span class="font-thin">Always in business.</span>
   </footer>
